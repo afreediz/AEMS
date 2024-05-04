59911bec34743e9a2f0cd5a64763a9f303ef7a0b
afreediz <afreedisulfiker@gmail.com>
2024-05-04 21:32:11 +0530

    ongoing base work

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,23 @@
-import { Route, Routes} from 'react-router-dom'
+import { Route, Routes, Outlet} from 'react-router-dom'
 import {ToastContainer } from 'react-toastify'
-import Home from './pages/Home'
+import Home from './pages/UIUX/Home/Home'
 import AdminProtected from './components/Route/AdminProtected'
 import Dashboard from './pages/Admin/Dashboard'
 import Login from './pages/Auth/Login'
 import ForgetPassword from './pages/Auth/ForegetPassword'
+import Layout from './components/UIUX/Layout'
+import Events from './pages/UIUX/Events/Events'
+import Contact from './pages/UIUX/Contact/Contact'
 
 const App = () => {
   return (
     <div className="">
       <Routes>
-        <Route index element={<Home />} />
+        <Route path='/' element={<Layout />} >
+          <Route index element={<Home />} />
+          <Route path='/events' element={<Events />} />
+          <Route path='/contact' element={<Contact />} />
+        </Route>
         <Route path='/login' element={<Login />}/>
         <Route path='/forget-password' element={<ForgetPassword />}/>
         <Route path='/admin' element={<AdminProtected />}>
